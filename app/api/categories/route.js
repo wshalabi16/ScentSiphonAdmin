fc4b65d308eae664bc2d4d838cc123e9fce09f36
wshalabi <wshalabi@sfu.ca>
2025-11-22 05:36:41 -0800

    editing and deleting categories, full implementation of parent categories

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -5,11 +5,10 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   await mongooseConnect();
   const { name, parentCategory } = await request.json();
-  const categoryData = { name };
-  if (parentCategory) {
-    categoryData.parent = parentCategory;
-  }
-  const categoryDoc = await Category.create(categoryData);
+  const categoryDoc = await Category.create({
+    name,
+    parent: parentCategory || null, 
+  });
   return NextResponse.json(categoryDoc);
 }
 
@@ -17,4 +16,20 @@ export async function GET() {
   await mongooseConnect();
   const categories = await Category.find().populate('parent');
   return NextResponse.json(categories);
+}
+
+export async function PUT(request) {
+  await mongooseConnect();
+  const { _id, name, parentCategory } = await request.json();
+  const categoryDoc = await Category.updateOne(
+    { _id },
+    { name,parent: parentCategory || null, });
+  return NextResponse.json(categoryDoc);
+}
+
+export async function DELETE(request) {
+  await mongooseConnect();
+  const id = request.nextUrl.searchParams.get('id');
+  await Category.deleteOne({ _id: id });
+  return NextResponse.json('ok');
 }
